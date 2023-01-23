46a88610c7b9a684b80eeb1515fa901abd18ba80
Michael Matiu <michaelmatiu@gmail.com>
2023-01-23 15:32:18 +0100

    put date start and end into period in mergetime

--- R/mergetime-eurocordex.R
+++ R/mergetime-eurocordex.R
@@ -5,12 +5,14 @@ library(fs)
 library(magrittr)
 library(lubridate)
 
-path_in <- "/home/climatedata/eurocordex/"
-path_out <- "/home/climatedata/eurocordex-merged/"
+path_in <- "/home/climatedata/eurocordex-temp2/"
+path_out <- "/home/climatedata/eurocordex/merged/"
 
 dat_inv <- get_inventory(path_in, T)
 dat_inv$variable %>% table
 dat_inv <- dat_inv[variable != "orog"]
+
+dat_inv[, period := paste0(format(date_start, "%Y%m%d"), "-", format(date_end, "%Y%m%d"))]
 
 for(i in 1:nrow(dat_inv)){
   
@@ -18,7 +20,7 @@ for(i in 1:nrow(dat_inv)){
   file_out <- path(path_out, 
                    dat_i$variable,
                    dat_i[, paste(variable, domain, gcm, experiment, ensemble, institute_rcm,
-                                 downscale_realisation, timefreq, date_start, date_end,
+                                 downscale_realisation, timefreq, period,
                                  sep = "_")],
                    ext = "nc")
   
