b2e0b7f2631cc8a55b598b0b04f7d98dbcbd44c5
AnnaNap92 <anna.napoli@unitn.it>
2023-03-28 17:24:15 +0200

    Calculation climate indices obs reviewed

--- bash/Climate_annual_indices_obs_P.sh
+++ bash/Climate_annual_indices_obs_P.sh
@@ -6,53 +6,73 @@
 #
 # Indices produced: CDD, SDII, RR1, R95pTOT
 #
-# 16th March, 2023
+# We didn't use the eca command in cdo for RR1, SDII, R95pTOT
+# because it destroys the time information in the netcdf files. 
+#
+# 24th March, 2023
 ##############################################################
 
-path_input='/home/climatedata/obs/regrid_data_0.95/SWITZERLAND';
+path_input='/home/climatedata/obs/regrid_data_0.95/CRESPI';
 path_output='/home/climatedata/obs/regrid_data_0.95/data_in_time/climate_indices';
-obs='SWITZERLAND';
-name_file_dataset='RhiresD_ch02h.lonlat_1961-2021_rg.nc'
+obs='CRESPI';
+name_file_dataset='DailySeries_1980_2018_Prec_rg.nc'
 variable='pr';
 
-first_year=1961;
-last_year=2021;
+first_year=1980;
+last_year=2018;
 
 #################################################
-#creation ofmaxe seasonal dataset:
+
+#index R95pTOT: calculation of the 95th percentile over the entire time series (only wet days)
+
+cp $path_input/${name_file_dataset} $path_output/copy_${name_file_dataset}
+cdo -ifthen -gtc,1 $path_output/copy_${name_file_dataset} $path_output/copy_${name_file_dataset} $path_output/wet_days_for_95th_percentile.nc               #here we are creating a file that contains only data when precipitation is greater then 1mm per day. If precipitation is less than 1mm, it is a NaN. (gtc creates the mask)
+cdo timpctl,95 $path_output/wet_days_for_95th_percentile.nc -timmin $path_output/wet_days_for_95th_percentile.nc -timmax $path_output/wet_days_for_95th_percentile.nc $path_output/95th_percentile.nc #percentile calculated only over wet days and over the entire time series
+
+rm $path_output/copy_${name_file_dataset}
+
+#creation of the indices at annual time scale:
 for year in $(eval echo "{$first_year..$last_year}")
 do
     cdo selyear,${year} $path_input/$name_file_dataset $path_output/${year}_daily.nc
 
-    #index 
-#    cdo eca_cdd $path_output/${year}_daily.nc $path_output/CDD_1_${year}.nc
-#    cdo eca_sdii $path_output/${year}_daily.nc $path_output/SII_1_${year}.nc
-#     cdo eca_rr1 $path_output/${year}_daily.nc $path_output/RR1_1_${year}.nc
-    #R95pTOT
-    cp $path_output/${year}_daily.nc $path_output/${year}_daily_forR95.nc                                                             #useful because we need to rewrtite on the same natcdf
-    cdo -ifthen -gtc,1 $path_output/${year}_daily_forR95.nc $path_output/${year}_daily_forR95.nc $path_output/${year}_daily_forR95_wetDays.nc #here we are creating a file that contains only data when precipitation is greater then 1mm per day. If precipitation is less than 1mm, it is a NaN. (gtc creates the mask)
-    cdo timpctl,95 $path_output/${year}_daily_forR95_wetDays.nc -timmin $path_output/${year}_daily_forR95_wetDays.nc -timmax $path_output/${year}_daily_forR95_wetDays.nc $path_output/${year}_percentile.nc #percentile for each year calculated only over wet days
-    cdo replace $path_output/${year}_daily_forR95_wetDays.nc $path_output/${year}_percentile.nc $path_output/${year}_percentile_moreTime.nc
-    cdo eca_r95ptot $path_output/${year}_daily_forR95_wetDays.nc $path_output/${year}_percentile_moreTime.nc $path_output/R95pTOT_1_${year}.nc
+    #index CDD
+    if [ $obs == "CRESPI" ]; then
+       cdo -setctomiss,0 -eca_cdd  $path_output/${year}_daily.nc $path_output/CDD_1_${year}.nc
+       else
+       cdo eca_cdd $path_output/${year}_daily.nc $path_output/CDD_1_${year}.nc
+    fi
+
+    #index SDII
+    cp $path_output/${year}_daily.nc $path_output/${year}_daily_for_RR1.nc  #because I need to write over the data daily
+    cdo -timmean -ifthen -gtc,1 $path_output/${year}_daily_for_RR1.nc $path_output/${year}_daily_for_RR1.nc $path_output/SDII_1_${year}.nc 
+
+    #index RR1
+    cdo -yearsum -gtc,1 $path_output/${year}_daily.nc $path_output/RR1_1_${year}.nc
+
+    #index R95pTOT
+    cp $path_output/${year}_daily.nc $path_output/${year}_daily_for_R95.nc  #because I need to write over the data daily
+    cdo -ifthen -gt $path_output/${year}_daily_for_R95.nc $path_output/95th_percentile.nc $path_output/${year}_daily_for_R95.nc $path_output/daily_wet_days_above_95th_${year}.nc  #select only days above the 95th percentile
+    cdo -div -yearsum $path_output/daily_wet_days_above_95th_${year}.nc -yearsum $path_output/${year}_daily.nc $path_output/R95pTOT_1_${year}.nc
 done
 
 #Creation annual dataset
 if [ $obs == "EOBS" ]; then
    echo "yep EOBS!"
-   rm $path_output/2022_*
+   rm $path_output/*_2022.nc
 fi
 if [ $obs == "v19HOM" ]; then
    echo "yep EOBS v19HOM!"
-   rm $path_output/2018_*
+   rm $path_output/*_2018.nc
 fi
 if [ $obs == "SPARTACUS" ]; then
    echo "yep SPARTACUS!"
-   rm $path_output/2022_*
+   rm $path_output/*_2022.nc
 fi
 if [ $obs == "HYRAS" ]; then
   	if [ $variable == "pr" ]; then
            echo "yep HYRAS!"
-           rm $path_output/2022_*
+           rm $path_output/*_2022.nc
         fi
 fi
 
@@ -63,15 +83,15 @@ rm $path_output/*percentile*
 
 if [ $obs == "v19HOM" ]; then
 
-  #  cdo mergetime $path_output/CDD_1_* $path_output/CDD_EOBSv19_annual.nc
-  #  cdo mergetime $path_output/SII_1_* $path_output/SII_EOBSv19_annual.nc
-  #  cdo mergetime $path_output/RR1_1_* $path_output/RR1_EOBSv19_annual.nc
+     cdo mergetime $path_output/CDD_1_* $path_output/CDD_EOBSv19_annual.nc
+     cdo mergetime $path_output/SDII_1_* $path_output/SDII_EOBSv19_annual.nc
+     cdo mergetime $path_output/RR1_1_* $path_output/RR1_EOBSv19_annual.nc
      cdo mergetime $path_output/R95pTOT_1_* $path_output/R95pTOT_EOBSv19_annual.nc 
 else
     
-  #  cdo mergetime $path_output/CDD_1_* $path_output/CDD_${obs}_annual.nc
-  #  cdo mergetime $path_output/SII_1_* $path_output/SII_${obs}_annual.nc
-  #  cdo mergetime $path_output/RR1_1_* $path_output/RR1_${obs}_annual.nc
+     cdo mergetime $path_output/CDD_1_* $path_output/CDD_${obs}_annual.nc
+     cdo mergetime $path_output/SDII_1_* $path_output/SDII_${obs}_annual.nc
+     cdo mergetime $path_output/RR1_1_* $path_output/RR1_${obs}_annual.nc
      cdo mergetime $path_output/R95pTOT_1_* $path_output/R95pTOT_${obs}_annual.nc
 fi
 
--- bash/Climate_annual_indices_obs_T.sh
+++ bash/Climate_annual_indices_obs_T.sh
@@ -9,45 +9,47 @@
 # 22th March, 2023
 ##############################################################
 
-path_input='/home/climatedata/obs/regrid_data_0.95/WFDEI';
+path_input='/home/climatedata/obs/regrid_data_0.95/EOBS/v19HOM';
 path_output='/home/climatedata/obs/regrid_data_0.95/data_in_time/climate_indices';
-obs='WFDEI';
-name_file_dataset='tasmax_wfdei_1981_2010_rg.nc'
-variable='tasmax';
+obs='v19HOM';
+name_file_dataset_Tmax='tx_ens_mean_0.1deg_GAR_reg_v19.0eHOM_rg.nc'
+name_file_dataset_Tmin='tn_ens_mean_0.1deg_GAR_reg_v19.0eHOM_rg.nc'
 
-first_year=1981;
-last_year=2010;
+first_year=1950;
+last_year=2018;
 
 #################################################
 #creation ofmaxe seasonal dataset:
 for year in $(eval echo "{$first_year..$last_year}")
 do
-    cdo -addc,273.15 -selyear,${year} $path_input/$name_file_dataset $path_output/${year}_daily.nc
+    cdo -addc,273.15 -selyear,${year} $path_input/$name_file_dataset_Tmax $path_output/${year}_daily_Tmax.nc
+    cdo -addc,273.15 -selyear,${year} $path_input/$name_file_dataset_Tmin $path_output/${year}_daily_Tmin.nc
 
-    #index     
-  #  cdo eca_fd $path_output/${year}_daily.nc $path_output/FD_1_${year}.nc   #TN (daily dataset) have to be given in units of Kelvin
- #   cdo eca_su $path_output/${year}_daily.nc $path_output/SU_1_${year}.nc   #TX (daily dataset) have to be given in units of Kelvin, the parameter T (eca_su,[T]) in Celsius
-   # cdo eca_tr $path_output/${year}_daily.nc $path_output/TR_1_${year}.nc   #TN (daily dataset) have to be given in units of Kelvin, the parameter T (eca_tr,[T]) in Celsius
-     cdo eca_id $path_output/${year}_daily.nc $path_output/ID_1_${year}.nc   #TX (daily dataset) have to be given in units of Kelvin
+    #indices     
+    cdo eca_fd $path_output/${year}_daily_Tmin.nc $path_output/FD_1_${year}.nc   #TN (daily dataset) have to be given in units of Kelvin
+    cdo eca_su $path_output/${year}_daily_Tmax.nc $path_output/SU_1_${year}.nc   #TX (daily dataset) have to be given in units of Kelvin, the parameter T (eca_su,[T]) in Celsius
+    cdo eca_tr $path_output/${year}_daily_Tmin.nc $path_output/TR_1_${year}.nc   #TN (daily dataset) have to be given in units of Kelvin, the parameter T (eca_tr,[T]) in Celsius
+    #The eca_id function modifies the time, so we calculate by ourselves
+    cdo -yearsum -ltc,273.15 $path_output/${year}_daily_Tmax.nc $path_output/ID_1_${year}.nc
 done
 
 #Creation annual dataset
 if [ $obs == "EOBS" ]; then
    echo "yep EOBS!"
-   rm $path_output/2022_*
+   rm $path_output/*_2022.nc
 fi
 if [ $obs == "v19HOM" ]; then
    echo "yep EOBS v19HOM!"
-   rm $path_output/2018_*
+   rm $path_output/*_2018.nc
 fi
 if [ $obs == "SPARTACUS" ]; then
    echo "yep SPARTACUS!"
-   rm $path_output/2022_*
+   rm $path_output/*_2022.nc
 fi
 if [ $obs == "HYRAS" ]; then
   	if [ $variable == "pr" ]; then
            echo "yep HYRAS!"
-           rm $path_output/2022_*
+           rm $path_output/*_2022.nc
         fi
 fi
 
@@ -56,15 +58,15 @@ rm $path_output/*daily*
 #Calculate the mean:
 
 if [ $obs == "v19HOM" ]; then
-    # cdo mergetime $path_output/FD_1_* $path_output/FD_EOBSv19_annual.nc
-    # cdo mergetime $path_output/SU_1_* $path_output/SU_EOBSv19_annual.nc 
-    # cdo mergetime $path_output/TR_1_* $path_output/TR_EOBSv19_annual.nc
+      cdo mergetime $path_output/FD_1_* $path_output/FD_EOBSv19_annual.nc
+      cdo mergetime $path_output/SU_1_* $path_output/SU_EOBSv19_annual.nc 
+      cdo mergetime $path_output/TR_1_* $path_output/TR_EOBSv19_annual.nc
       cdo mergetime $path_output/ID_1_* $path_output/ID_EOBSv19_annual.nc
 else
-    # cdo mergetime $path_output/FD_1_* $path_output/FD_${obs}_annual.nc
-    # cdo mergetime $path_output/SU_1_* $path_output/SU_${obs}_annual.nc
-    # cdo mergetime $path_output/TR_1_* $path_output/TR_${obs}_annual.nc
-     cdo mergetime $path_output/ID_1_* $path_output/ID_${obs}_annual.nc
+      cdo mergetime $path_output/FD_1_* $path_output/FD_${obs}_annual.nc
+      cdo mergetime $path_output/SU_1_* $path_output/SU_${obs}_annual.nc
+      cdo mergetime $path_output/TR_1_* $path_output/TR_${obs}_annual.nc
+      cdo mergetime $path_output/ID_1_* $path_output/ID_${obs}_annual.nc
 fi
 
 rm $path_output/*_1_*
