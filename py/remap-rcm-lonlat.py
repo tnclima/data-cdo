48588f94f8f602bc47c3e2c713837929e4598a3b
Michael Matiu <michaelmatiu@gmail.com>
2023-10-04 11:44:19 +0200

    remap lonlat TNAA (rcm, crespi_1km)

---
+++ py/remap-rcm-lonlat.py
@@ -0,0 +1,42 @@
+# remap to regular lonlat and crop RCMs as specified by coords file
+import os
+import glob
+from cdo import Cdo
+
+cdo = Cdo()
+
+file_coords = "/home/michael.matiu/projects/downscaling/data/coords_tnaa.txt"
+
+# orog
+path_in = "/home/climatedata/eurocordex2-rest/orog/"
+path_out = "/home/climatedata/downscaling/rcm_lonlat_tnaa/orog/"
+os.makedirs(path_out, exist_ok=True)
+
+all_files = os.listdir(path_in)
+all_files.sort()
+
+for j,file_loop in enumerate(all_files):
+  
+  file_in = os.path.join(path_in, file_loop)
+  file_out = os.path.join(path_out, file_loop)
+  cdo.remapbil(file_coords, input=file_in, output=file_out)
+
+
+# other vars
+path_in = "/home/climatedata/eurocordex2-rest/merged/"
+path_out = "/home/climatedata/downscaling/rcm_lonlat_tnaa/"
+
+variables = ["tas", "tasmin", "tasmax", "pr"]
+
+for var in variables:
+
+    all_files = os.listdir(os.path.join(path_in, var))
+    all_files.sort()
+    os.makedirs(os.path.join(path_out, var), exist_ok=True)
+    
+    for j,file_loop in enumerate(all_files):
+  
+        file_in = os.path.join(path_in, var, file_loop)
+        file_out = os.path.join(path_out, var, file_loop)
+        cdo.remapbil(file_coords, input=file_in, output=file_out)
+
