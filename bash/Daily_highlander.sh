07401c439d4485aa9e25f960ae567aceaf2c6ed9
AnnaNap92 <anna.napoli@unitn.it>
2024-09-25 11:39:52 +0200

    Creation of daily mean (or max and min) for highlander

--- bash/Daily_highlander.sh
+++ bash/Daily_highlander.sh
@@ -1,45 +1,98 @@
-########################################################################
-# This script calculates the daily precipitation from hourly highlander
-# dataset
+#######################################################################
+# This script calculates the daily precipitation and temperature from 
+# hourly highlander dataset
 
 # 8 June, 2023   Anna Napoli
 ########################################################################
-path_output='/home/climatedata/highlander/Daily/rcp'
-path_input='/home/climatedata/highlander/hourly/historical_1981-2005'
+path_output='/home/climatedata/climate_scenarios_appa/dati_spaziotemporali/vhr-pro-it-2km_giornaliero'
+path_input='/home/climatedata/climate_scenarios_appa/dati_spaziotemporali/vhr-pro-it-2km_orario'
 ##########################################################################
 CDO_RESET_HISTORY=1 ;
 export CDO_RESET_HISTORY
 export REMAP_AREA_MIN=0.95
 
 # Now the loop starts:
-count=0
+# HISTORICAL
 for year in {1981..2005} #2005
 do
-        for month in {1..12} #12 #`seq -w 8 12`  #01 02 03 04 05 06 07 08 09 10 11 12
-        do
-		var_mon=$(printf %02d $month)
+	echo "$year"
+
+	#Precipitation
+	cdo daysum $path_input/pr/pr_historical_${year}_cmcc2km_tn.nc $path_output/pr/provaP.nc
+	
+	#Temperature
+	cdo -addc,-273.15 -daymean $path_input/tas/tas_historical_${year}_cmcc2km_tn.nc $path_output/tas/provaT.nc
+        cdo -addc,-273.15 -daymax $path_input/tas/tas_historical_${year}_cmcc2km_tn.nc $path_output/tasmax/provaTx.nc
+        cdo -addc,-273.15 -daymin $path_input/tas/tas_historical_${year}_cmcc2km_tn.nc $path_output/tasmin/provaTn.nc
+	
+	#Creation of the data:
+	#Precipitation
+        cdo -chname,TOT_PREC,pr -setattribute,TOT_PREC@units="mm" $path_output/pr/provaP.nc $path_output/pr/pr_historical_${year}_cmcc2km_tn.nc
+	rm $path_output/pr/provaP.nc
 		
-		#Precipitation
-		cdo daysum $path_input/precipitation/climate-projections-downscaled-over-italy-hourly-precipitation_amount-${year}-${var_mon}.nc $path_output/provaP.nc
+	#Temperature
+	cdo chname,T_2M,tas -setattribute,T_2M@units="C" $path_output/tas/provaT.nc $path_output/tas/tas_historical_${year}_cmcc2km_tn.nc
+	cdo chname,T_2M,tasmax -setattribute,T_2M@units="C" $path_output/tasmax/provaTx.nc $path_output/tasmax/tasmax_historical_${year}_cmcc2km_tn.nc
+	cdo chname,T_2M,tasmin -setattribute,T_2M@units="C" $path_output/tasmin/provaTn.nc $path_output/tasmin/tasmin_historical_${year}_cmcc2km_tn.nc
+	
+	rm $path_output/tas/provaT.nc
+	rm $path_output/tasmin/provaTn.nc
+	rm $path_output/tasmax/provaTx.nc
+done
 
-		#Temperature
-		cdo -addc,-273.15 -daymean $path_input/temperature/climate-projections-downscaled-over-italy-hourly-air_temperature-${year}-${var_mon}.nc $path_output/provaT.nc
-                cdo -addc,-273.15 -daymax $path_input/temperature/climate-projections-downscaled-over-italy-hourly-air_temperature-${year}-${var_mon}.nc $path_output/provaTx.nc
-                cdo -addc,-273.15 -daymin $path_input/temperature/climate-projections-downscaled-over-italy-hourly-air_temperature-${year}-${var_mon}.nc $path_output/provaTm.nc
+#RCP4.5
+for year in {2006..2070} #2005
+do
+	echo "$year"
 
-		#Creation of the data:
-		#Precipitation
-                cdo -chname,TOT_PREC,pr -setattribute,TOT_PREC@units="mm" $path_output/provaP.nc $path_output/rcp45_pr_${year}_${var_mon}.nc
-		rm $path_output/provaP.nc
-		
-		#Temperature
-		cdo chname,T_2M,tas -setattribute,T_2M@units="C" $path_output/provaT.nc $path_output/rcp45_tas_${year}_${var_mon}.nc
-                cdo chname,T_2M,tasmax -setattribute,T_2M@units="C" $path_output/provaTx.nc $path_output/rcp45_tasmax_${year}_${var_mon}.nc
-                cdo chname,T_2M,tasmin -setattribute,T_2M@units="C" $path_output/provaTm.nc $path_output/rcp45_tasmin_${year}_${var_mon}.nc
+	#Precipitation
+        cdo daysum $path_input/pr/pr_rcp45_${year}_cmcc2km_tn.nc $path_output/pr/provaP.nc
 
-		rm $path_output/provaT.nc
- 		rm $path_output/provaTm.nc
-		rm $path_output/provaTx.nc
-	done
+        #Temperature
+        cdo -addc,-273.15 -daymean $path_input/tas/tas_rcp45_${year}_cmcc2km_tn.nc $path_output/tas/provaT.nc
+        cdo -addc,-273.15 -daymax $path_input/tas/tas_rcp45_${year}_cmcc2km_tn.nc $path_output/tasmax/provaTx.nc
+        cdo -addc,-273.15 -daymin $path_input/tas/tas_rcp45_${year}_cmcc2km_tn.nc $path_output/tasmin/provaTn.nc
+
+        #Creation of the data:
+        #Precipitation
+        cdo -chname,TOT_PREC,pr -setattribute,TOT_PREC@units="mm" $path_output/pr/provaP.nc $path_output/pr/pr_rcp45_${year}_cmcc2km_tn.nc
+        rm $path_output/pr/provaP.nc
+
+        #Temperature
+        cdo chname,T_2M,tas -setattribute,T_2M@units="C" $path_output/tas/provaT.nc $path_output/tas/tas_rcp45_${year}_cmcc2km_tn.nc
+        cdo chname,T_2M,tasmax -setattribute,T_2M@units="C" $path_output/tasmax/provaTx.nc $path_output/tasmax/tasmax_rcp45_${year}_cmcc2km_tn.nc
+        cdo chname,T_2M,tasmin -setattribute,T_2M@units="C" $path_output/tasmin/provaTn.nc $path_output/tasmin/tasmin_rcp45_${year}_cmcc2km_tn.nc
+
+        rm $path_output/tas/provaT.nc
+        rm $path_output/tasmin/provaTn.nc
+        rm $path_output/tasmax/provaTx.nc
+done
+
+#RCP8.5
+for year in {2006..2070} #2005
+do
+        echo "$year"
+
+        #Precipitation
+        cdo daysum $path_input/pr/pr_rcp85_${year}_cmcc2km_tn.nc $path_output/pr/provaP.nc
+
+        #Temperature
+        cdo -addc,-273.15 -daymean $path_input/tas/tas_rcp85_${year}_cmcc2km_tn.nc $path_output/tas/provaT.nc
+        cdo -addc,-273.15 -daymax $path_input/tas/tas_rcp85_${year}_cmcc2km_tn.nc $path_output/tasmax/provaTx.nc
+        cdo -addc,-273.15 -daymin $path_input/tas/tas_rcp85_${year}_cmcc2km_tn.nc $path_output/tasmin/provaTn.nc
+
+        #Creation of the data:
+        #Precipitation
+        cdo -chname,TOT_PREC,pr -setattribute,TOT_PREC@units="mm" $path_output/pr/provaP.nc $path_output/pr/pr_rcp85_${year}_cmcc2km_tn.nc
+        rm $path_output/pr/provaP.nc
+
+        #Temperature
+        cdo chname,T_2M,tas -setattribute,T_2M@units="C" $path_output/tas/provaT.nc $path_output/tas/tas_rcp85_${year}_cmcc2km_tn.nc
+        cdo chname,T_2M,tasmax -setattribute,T_2M@units="C" $path_output/tasmax/provaTx.nc $path_output/tasmax/tasmax_rcp85_${year}_cmcc2km_tn.nc
+        cdo chname,T_2M,tasmin -setattribute,T_2M@units="C" $path_output/tasmin/provaTn.nc $path_output/tasmin/tasmin_rcp85_${year}_cmcc2km_tn.nc
+
+        rm $path_output/tas/provaT.nc
+        rm $path_output/tasmin/provaTn.nc
+        rm $path_output/tasmax/provaTx.nc
 done
 
